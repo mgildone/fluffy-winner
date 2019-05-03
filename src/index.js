2c1037cdff4bc670981cef8e9d38062503c9634e
Matteo Gildone <matteo.gildone@outlook.com>
2019-05-03 08:19:44 +0000

    add Reach/Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Link } from "@reach/router"
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="dashboard">Dashboard</Link>
+    </nav>
+  </div>
+)
+
+const Dash = () => <div>Dash</div>
+
+ReactDOM.render(<Router>
+    <Home path="/" />
+    <Dash path="dashboard" />
+  </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
