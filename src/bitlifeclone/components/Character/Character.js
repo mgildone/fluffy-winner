50d0a26983edc8bbcc73fad9798a00f1feee4fa7
Matteo Gildone <matteo.gildone@outlook.com>
2019-05-05 21:21:46 +0100

    restyle

--- src/bitlifeclone/components/Character/Character.js
+++ src/bitlifeclone/components/Character/Character.js
@@ -5,24 +5,40 @@ import "./Character.scss";
 const Character = props => {
   return (
     <dl className="c-character">
-      <dt>Name:</dt>
-      <dd>
-        {props.character.firstName} {props.character.lastName}
-      </dd>
-      <dt>Nationality:</dt>
-      <dd>{props.character.nationality.name}</dd>
-      <dt>Gender:</dt>
-      <dd>{props.character.gender.name}</dd>
-      <dt>Sexual orientation:</dt>
-      <dd>{props.character.sexualOrientation}</dd>
-      <dt>Happiness</dt>
-      <dd>{props.character.happiness}</dd>
-      <dt>Health</dt>
-      <dd>{props.character.health}</dd>
-      <dt>Smarts</dt>
-      <dd>{props.character.smarts}</dd>
-      <dt>Look</dt>
-      <dd>{props.character.looks}</dd>
+      <div>
+        <dt>Name</dt>
+        <dd>
+          {props.character.firstName} {props.character.lastName}
+        </dd>
+      </div>
+      <div>
+        <dt>Nationality</dt>
+        <dd>{props.character.nationality.name}</dd>
+      </div>
+      <div>
+        <dt>Gender</dt>
+        <dd>{props.character.gender.name}</dd>
+      </div>
+      <div>
+        <dt>Sexual orientation</dt>
+        <dd>{props.character.sexualOrientation}</dd>
+      </div>
+      <div>
+        <dt>Happiness</dt>
+        <dd>{props.character.happiness}</dd>
+      </div>
+      <div>
+        <dt>Health</dt>
+        <dd>{props.character.health}</dd>
+      </div>
+      <div>
+        <dt>Smarts</dt>
+        <dd>{props.character.smarts}</dd>
+      </div>
+      <div>
+        <dt>Look</dt>
+        <dd>{props.character.looks}</dd>
+      </div>
     </dl>
   );
 };
